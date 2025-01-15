4d5c0dd4511d7c24de5a53ea450198d163f7cdea
Federico Dip <federicodiplp@gmail.com>
2025-01-15 02:20:57 -0300

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import LanguageWrapper from '@/components/LanguageWrapper';
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
       </LanguageWrapper>
     </html>
